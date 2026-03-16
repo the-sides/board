83b5a6f3480cfcd7418146f08d3e8a3570b332de
Jacob Sides <jacob.r.sides@gmail.com>
2026-03-15 20:00:54 -0400

    breakout words in Griflan

--- src/routes/demo/griflan.tsx
+++ src/routes/demo/griflan.tsx
@@ -22,24 +22,24 @@ function GriflanPage() {
     tl.set(el, { width: 0 })
     tl.set('p', { opacity: 0 })
     tl.set('h1', { opacity: 0 })
-    tl.to(el, { width: '50vw', duration: 1})
+    tl.to(el, { width: '50vw', duration: 1 })
     tl.to('p', { opacity: 1 }, 1)
     tl.to('h1', { opacity: 1 }, 2)
   })
 
   return (
-    <main className="text-amber-100 flex items-center justify-center h-screen">
-      <div className="center-wrapper">
+    <main className="text-amber-100 flex items-center justify-center min-h-screen">
+      <div className="center-wrapper absolute inset-0 m-auto flex items-center justify-center flex-col">
         <div className="top text-4xl flex justify-center items-center gap-x-6">
           <p className='opacity-0'>Grif</p>
           <div className="w-[50vw] flex items-center">
-          <div ref={lineRef} className="mx-auto w-0 line border-t border-amber-100 opacity-50"></div>
+            <div ref={lineRef} className="mx-auto w-0 line border-t border-amber-100 opacity-50"></div>
           </div>
           <p className='opacity-0'>lan</p>
         </div>
-        <div className="bottom w-full text-center mt-12">
-          <h1 className="opacity-0 text-8xl ">Bringing Ideas to Life</h1>
-        </div>
+      </div>
+      <div className="bottom w-full text-center pt-48">
+        <h1 className="opacity-0 text-4xl max-w-screen-md mx-auto"><span>Bringing</span> <span className="opacity-0"> details together to formulate</span> <span className="">Ideas</span> <span className="opacity-0">that communicate your brand</span> <span className="">to</span> <span className="opacity-0">the customers that bring your company</span> <span className="">Life</span></h1>
       </div>
     </main>
   )
